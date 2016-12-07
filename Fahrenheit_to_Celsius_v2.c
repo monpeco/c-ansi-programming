2679a4328678aa1edbe5d898d44477c85fd9a920
monpeco <monpeco@gmail.com>
2016-12-07 12:39:03 +0000

    Update on Fahr_to_Cel, add Symbolic Constant

--- Fahrenheit_to_Celsius_v2.c
+++ Fahrenheit_to_Celsius_v2.c
@@ -1,11 +1,15 @@
 #include <stdio.h>
 
+#define LOWER 0		/* Lower limit of table */
+#define UPPER 300	/* Upper limit of table */
+#define STEP  20	/* Step size */
+
 /*print Fahrenheit-Celsiud table*/
 
 main()
 {
 	int fahr;
 	
-	for(fahr=0; fahr<=300; fahr = fahr + 20)
+	for(fahr=LOWER; fahr<=UPPER; fahr = fahr + STEP)
 	    printf("%3d %6.1f\n", fahr, (5.0/9.0)*(fahr-32));
 }
