8a519470bef7bbb5c71c43f5e7c7f10203156517
monpeco <monpeco@gmail.com>
2016-12-08 17:28:01 +0000

    Update on Fahr_to_Cel, add return value to main

--- Fahrenheit_to_Celsius_v2.c
+++ Fahrenheit_to_Celsius_v2.c
@@ -6,10 +6,12 @@
 
 /*print Fahrenheit-Celsiud table*/
 
-main()
+int main()
 {
 	int fahr;
 	
 	for(fahr=LOWER; fahr<=UPPER; fahr = fahr + STEP)
 	    printf("%3d %6.1f\n", fahr, (5.0/9.0)*(fahr-32));
+	
+	return 0;
 }
