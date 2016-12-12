5b2f27f486ca1e0076fcee08aca176660b06bc8f
monpeco <monpeco@gmail.com>
2016-12-12 15:02:55 +0000

    Update on getline, implementation of copy, and printing longest

--- get_line.c
+++ get_line.c
@@ -15,9 +15,13 @@ int main(){
     while((len = getLine(line, MAXLINE)) > 0){
         if(len > max){
             max = len;
+            copy(longest, line);
         }
     }
     
+    if(max > 0){
+        printf("%s", longest);
+    }
     
     return 0;
 }
@@ -35,4 +39,14 @@ int getLine(char s[], int limit){
     s[i] = '\0';
     
     return i;
+}
+
+void copy(char to[], char from[]){
+    int i;
+    
+    i = 0;
+    while((to[i] = from[i]) != '\0'){
+        ++i;
+    }
+    
 }
