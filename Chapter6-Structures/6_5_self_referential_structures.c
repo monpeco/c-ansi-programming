554a39f960959dded9023ef1afc266bbd2df25bf
monpeco <monpeco@gmail.com>
2019-02-03 19:47:52 +0000

    Create tnode struct to binary tree - self-referential structures

---
+++ Chapter6-Structures/6_5_self_referential_structures.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+struct tnode {        /* The tree node */
+  char *word;         /* pointer to the text */
+  int count;          /* number of occurrnces */
+  struct tnode *left;  /* pointer to the left child */
+  struct tnode *right; /* pointer to the right child */
+};
+
+int main(int argc, char *argv[]){
+  
+  
+  return 0;
+}
