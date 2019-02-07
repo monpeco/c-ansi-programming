9d3842ba090ffe35fd7d19f56d6277384046cb7c
monpeco <monpeco@gmail.com>
2019-02-07 21:51:43 +0000

    Add getword declaration and (partial) definition

--- Chapter6-Structures/6_5_self_referential_structures.c
+++ Chapter6-Structures/6_5_self_referential_structures.c
@@ -1,4 +1,8 @@
 #include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+
+#define MAXWORD 100
 
 struct tnode {        /* The tree node */
   char *word;         /* pointer to the text */
@@ -7,8 +11,21 @@ struct tnode {        /* The tree node */
   struct tnode *right; /* pointer to the right child */
 };
 
+struct tnode *addtree(struct tnode *, char *);
+int getword(char *, int);
+
 int main(int argc, char *argv[]){
   
+  
+  return 0;
+}
+
+/* getword: get the next word or character from imput */
+int getword(char *word, int lim)
+{
+  int c, getch(void);
+  void ungetch(int);
+  char *w = word;
   
   return 0;
 }
